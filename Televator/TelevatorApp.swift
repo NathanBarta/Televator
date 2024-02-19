9b5a85e700c58c2188a1e715390e629cf1bd958c
Nathan Barta <71276691+NathanBarta@users.noreply.github.com>
2024-02-19 14:25:03 -0500

    Initial Commit

---
+++ Televator/TelevatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelevatorApp.swift
+//  Televator
+//
+//  Created by Nathan Barta on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct TelevatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
