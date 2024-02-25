534857b43333ed9491d64bf4ead44ce18133623e
Nathan Barta <71276691+NathanBarta@users.noreply.github.com>
2024-02-25 15:28:05 -0500

    Remove old code, Add rolling avg, Add manual elevator enter/exit

--- Televator/MainView.swift
+++ Televator/MainView.swift
@@ -10,15 +10,10 @@ import Charts
 
 struct MainView: View {
   @StateObject var viewModel = MainViewViewModel()
+  @State var isTapped = false
     
   var body: some View {
     VStack(spacing: 16.0) {
-      //      gps
-      //      accelerometer
-      //      magnet
-      //      gravity
-      //      barometric
-      
       chart
 
       Text(viewModel.errorString)
@@ -26,62 +21,52 @@ struct MainView: View {
     }
     .padding()
   }
-  
-  var gps: some View {
-    VStack {
-      Text("GPS Radius of Uncertainty")
-      Text(viewModel.gpsSignalStrength.description)
-      Text(viewModel.gpsTimeSinceLastUpdate.description)
-    }
-  }
-  
-  var accelerometer: some View {
-    VStack {
-      Text("(Z-axis) Accelerometer")
-      Text(String(format: "%.3f", viewModel.zAcceleration))
-    }
-  }
-  
-  var magnet: some View {
-    VStack {
-      Text("Magnetometer")
-      Text(String(format: "%.3f", viewModel.mMagnetometer))
-    }
-  }
-  
-  var gravity: some View {
-    VStack {
-      Text("Gravity")
-      Text(String(format: "%.3f", viewModel.zGravity))
-    }
-  }
-  
-  var barometric: some View {
-    VStack {
-      Text("Barometric Pressure")
-      Text(String(format: "%.3f", viewModel.barometric))
-    }
-  }
-  
+
   var chart: some View {
     Chart {
-      RuleMark(y: .value("Limit", 0.5))
-        .foregroundStyle(.green)
+      if let rollingAvg = viewModel.rollingAvg {
+        RuleMark(y: .value("Rolling Avg", rollingAvg))
+          .foregroundStyle(.green)
+      }
       
-      ForEach(Array(viewModel.latencyHistory.enumerated()), id: \.offset) { index, value in
-        LineMark(
-          x: .value("Sample", index),
-          y: .value("Latency", value)
+      if let manualEnterElevatorIndex = viewModel.manualEnterElevatorIndex {
+        RuleMark(
+          x: .value("manualEnterElevatorTimesamp", manualEnterElevatorIndex),
+          yStart: .value("manualEnterElevatorTimesamp", 0),
+          yEnd: .value("manualEnterElevatorTimestamp", 0.5)
         )
-        
-        // Pretty intensive
-        //        AreaMark(
-        //          x: .value("Sample", index),
-        //          y: .value("Latency", value)
-        //        )
-        //        .foregroundStyle(.blue.opacity(0.8))
+        .foregroundStyle(.red)
+      }
+      
+      if let manualExitElevatorIndex = viewModel.manualExitElevatorIndex {
+        RuleMark(
+          x: .value("manualEnterElevatorTimesamp", manualExitElevatorIndex),
+          yStart: .value("manualEnterElevatorTimesamp", 0),
+          yEnd: .value("manualEnterElevatorTimestamp", 0.5)
+        )
+        .foregroundStyle(.red)
+      }
+      
+      ForEach(viewModel.latencyHistory, id: \.id) { record in
+        LineMark(
+          x: .value("Sample", record.id),
+          y: .value("Latency", record.latency)
+        )
       }
     }
+    .simultaneousGesture(
+      DragGesture(minimumDistance: 0)
+        .onChanged { _ in
+          if !self.isTapped {
+            self.isTapped = true
+            self.viewModel.addManualEnterElevator()
+          }
+        }
+        .onEnded { _ in
+          self.isTapped = false
+          self.viewModel.addManualExitElevator()
+        }
+    )
   }
 }
 
--- Televator/MainViewViewModel.swift
+++ Televator/MainViewViewModel.swift
@@ -12,117 +12,68 @@ import CoreTelephony
 import Combine
 import SwiftyPing
 
+let PING_INTERVAL: TimeInterval = 1.0
+let WINDOW_WIDTH: Int = 10
+
+public struct LatencyRecord: Identifiable {
+  public let id: Int
+  public let latency: TimeInterval
+}
+
 final public class MainViewViewModel: NSObject, ObservableObject {
-  @Published var gpsSignalStrength: Double = .zero
-  @Published var gpsTimeSinceLastUpdate: TimeInterval = .zero
-  @Published var zAcceleration: Double = .zero
-  @Published var mMagnetometer: Double = .zero
-  @Published var zGravity: Double = .zero
-  @Published var barometric: Double = .zero
-  @Published var latencyHistory: [TimeInterval] = .init()
+  @Published var latencyHistory: [LatencyRecord] = .init()
+  @Published var rollingAvg: TimeInterval? = nil
+  @Published var manualEnterElevatorIndex: Int?
+  @Published var manualExitElevatorIndex: Int?
   
-  private var lastTimeStamp: Date = .distantPast {
-    willSet(newTimeStamp) {
-      gpsTimeSinceLastUpdate = newTimeStamp.timeIntervalSince(lastTimeStamp)
-    }
-  }
+  private var windowSum: TimeInterval = .zero
+  private var rollingSum: TimeInterval = .zero
+  private var sampleIndex: Int = -1
   
   @Published var errorString: String = .init()
 
-  private lazy var locationManager = CLLocationManager()
-  private lazy var motionManager = CMMotionManager()
-  private lazy var altimeterManager = CMAltimeter()
-  
   override init() {
     super.init()
 
-    locationManager.delegate = self
-    locationManager.allowsBackgroundLocationUpdates = true
-    locationManager.showsBackgroundLocationIndicator = true
-    locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
-    locationManager.pausesLocationUpdatesAutomatically = false
-    locationManager.activityType = .otherNavigation // .fitness also pretty good
-        
-    switch locationManager.authorizationStatus {
-      case .authorizedWhenInUse:
-        locationManager.requestAlwaysAuthorization()
-        break
-      case .notDetermined:
-        locationManager.requestAlwaysAuthorization()
-        break
-      case .denied, .restricted:
-        fatalError("Location denied")
-        break
-      default:
-        break
-    }
-    
-    switch locationManager.accuracyAuthorization {
-      case .fullAccuracy:
-        print("isAuthorizedForPreciseLocation = true")
-      case .reducedAccuracy:
-        print("isAuthorizedForPreciseLocation = false")
-      @unknown default:
-        fatalError("Unknown accuracy authorization")
-    }
-    
-    var config = PingConfiguration(interval: 1.0, with: 5.0)
+    var config = PingConfiguration(interval: PING_INTERVAL, with: 5.0)
     config.handleBackgroundTransitions = true
     
     let pinger = try? SwiftyPing(host: "1.1.1.1", configuration: config, queue: DispatchQueue.global())
     pinger?.observer = { (response) in
-      let duration = response.duration
       DispatchQueue.main.async {
-        self.latencyHistory.append(duration)
+        self.addLatencyReading(response)
       }
-      print(duration)
     }
     
     pinger?.finished = { (result) in
-      print("Result")
+      print(result)
     }
 
     try? pinger?.startPinging()
-    
-//    locationManager.startUpdatingLocation()
-    
-//    motionManager.startAccelerometerUpdates(to: .main) { data, error in
-//      if let data = data {
-//        self.zAcceleration = -9.81 * data.acceleration.z
-//      }
-//    }
-    
-//    motionManager.startMagnetometerUpdates(to: .main) { data, error in
-//      if let data = data {
-//        self.mMagnetometer = max(data.magneticField.x, data.magneticField.y, data.magneticField.z, .zero)
-//      }
-//    }
-//    
-//    motionManager.startDeviceMotionUpdates(to: .main) { data, error in
-//      if let data = data {
-//        self.zGravity = data.gravity.z
-//      }
-//    }
-//
-//    altimeterManager.startRelativeAltitudeUpdates(to: .main) { data, error in
-//      if let data = data {
-//        self.barometric = data.pressure.doubleValue
-//      }
-//    }
-  }
-}
-
-// MARK: - Location
-extension MainViewViewModel: CLLocationManagerDelegate {
-  public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    gpsSignalStrength = locations.last!.horizontalAccuracy
-    lastTimeStamp = .now
-    print(gpsSignalStrength.description)
   }
   
-  public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-    locationManager.stopUpdatingLocation()
-    errorString = error.localizedDescription
-    print(errorString)
+  private func addLatencyReading(_ p: PingResponse) {
+    let sequenceNumber = p.sequenceNumber
+    let duration = p.duration
+
+    sampleIndex += 1
+    latencyHistory.append(.init(id: Int(sequenceNumber), latency: duration))
+    #if DEBUG
+      print(sequenceNumber, duration)
+    #endif
+    
+    rollingSum += duration
+    if sequenceNumber > WINDOW_WIDTH {
+      rollingSum -= latencyHistory[Int(sequenceNumber) - WINDOW_WIDTH].latency
+      rollingAvg = rollingSum / Double(WINDOW_WIDTH)
+    }
+  }
+  
+  public func addManualEnterElevator() {
+    manualEnterElevatorIndex = sampleIndex
+  }
+  
+  public func addManualExitElevator() {
+    manualExitElevatorIndex = sampleIndex
   }
 }
